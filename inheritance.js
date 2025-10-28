14f26ca1dcbe5c282e75b38ad165af31ccb17c7b
RenggaX <gamingtube1985@gmail.com>
2025-10-28 20:35:55 +0800

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,29 @@
-console.log("Inheritance in JavaScript");
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    introduceSelf() {
+        console.log(`Hello, my name is ${this.name}.`);
+    }
+}
+
+class Proferssor extends Person {
+    teaches;
+
+    constructor(name, teaches) {
+        super(name);
+        this.teaches = teaches;
+    }
+    introduceSelf() {
+        console.log(`my name is ${this.name}, and I will be your ${this.teaches} professor this semester.`);
+    }
+
+grade(paper) {
+    const grade = Math.floor(Math.random() * (5 - 1) + 1);
+    console.log(grade);
+    }
+}
+const walsh = new Proferssor("Walsh", "History");
+walsh.introduceSelf(); // menampilkan perkenalan profesor Walsh
+
+walsh.grade("my paper"); // menampilkan nilai acak antara 1-5 untuk paper siswa
